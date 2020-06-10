ce4058e727b549bc6ee8b598c917205fb9bf60c0
zawadka <louis.78100@hotmail.fr>
2020-06-10 19:58:28 +0200

    add hashpassword via PostMan

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -5,11 +5,19 @@ namespace App\Entity;
 use ApiPlatform\Core\Annotation\ApiResource;
 use App\Repository\UserRepository;
 use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 /**
- * @ApiResource()
+ * @ApiResource(
+        itemOperations={"get"},
+ *      collectionOperations={"post"},
+ *      normalizationContext={
+ *        "groups"={"read"}
+ *      }
+ *   )
  * @ORM\Entity(repositoryClass=UserRepository::class)
  */
 class User implements UserInterface
@@ -18,21 +26,25 @@ class User implements UserInterface
    * @ORM\Id()
    * @ORM\GeneratedValue()
    * @ORM\Column(type="integer")
+   * @Groups({"read"})
    */
   private $id;
 
   /**
    * @ORM\Column(type="string", length=255)
+   * @Groups({"read"})
    */
   private $username;
 
   /**
    * @ORM\Column(type="string", length=255)
+   *
    */
   private $password;
 
   /**
    * @ORM\Column(type="string", length=255)
+   * @Groups({"read"})
    */
   private $name;
 
@@ -43,11 +55,13 @@ class User implements UserInterface
 
   /**
    * @ORM\OneToMany(targetEntity="App\Entity\BlogPost", mappedBy="author")
+   * @Groups({"read"})
    */
   private $posts;
 
   /**
    * @ORM\OneToMany(targetEntity="App\Entity\Comment", mappedBy="author")
+   * @Groups({"read"})
    */
   private $comments;
 
@@ -111,17 +125,17 @@ class User implements UserInterface
   }
 
   /**
-   * @return ArrayCollection
+   * @return Collection
    */
-  public function getPosts(): ArrayCollection
+  public function getPosts(): Collection
   {
     return $this->posts;
   }
 
   /**
-   * @return ArrayCollection
+   * @return Collection
    */
-  public function getComments(): ArrayCollection
+  public function getComments(): Collection
   {
     return $this->comments;
   }
@@ -143,7 +157,7 @@ class User implements UserInterface
    */
   public function getRoles()
   {
-    return ['ROLE_USER'];
+    return ['USER'];
   }
 
   /**

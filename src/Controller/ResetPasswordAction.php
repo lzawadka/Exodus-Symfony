503c14271d787d6da224592dc1332b6a83e41d27
zawadka <louis.78100@hotmail.fr>
2020-06-25 17:15:28 +0200

    Update reset password

--- src/Controller/ResetPasswordAction.php
+++ src/Controller/ResetPasswordAction.php
@@ -6,6 +6,10 @@ namespace App\Controller;
 
 use ApiPlatform\Core\Validator\ValidatorInterface;
 use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class ResetPasswordAction
 {
@@ -13,10 +17,30 @@ class ResetPasswordAction
    * @var ValidatorInterface
    */
   private $validator;
+  /**
+   * @var UserPasswordEncoderInterface
+   */
+  private $userPasswordEncoder;
+  /**
+   * @var EntityManagerInterface
+   */
+  private $entityManager;
+  /**
+   * @var JWTTokenManagerInterface
+   */
+  private $tokenManager;
 
-  public function __construct(ValidatorInterface $validator)
+  public function __construct(
+    ValidatorInterface $validator,
+    UserPasswordEncoderInterface $userPasswordEncoder,
+    EntityManagerInterface $entityManager,
+  JWTTokenManagerInterface $tokenManager
+)
   {
     $this->validator = $validator;
+    $this->userPasswordEncoder = $userPasswordEncoder;
+    $this->entityManager = $entityManager;
+    $this->tokenManager = $tokenManager;
   }
 
   public function __invoke(User $data)
@@ -26,6 +50,18 @@ class ResetPasswordAction
     // var_dump($data->getNewPassword(), $data->getOldPassword());die;
     $this->validator->validate($data);
 
-    return $data;
+    $data->setPassword(
+      $this->userPasswordEncoder->encodePassword(
+        $data, $data->getNewPassword()
+      )
+    );
+
+    $data->setPasswordChangeDate(time());
+
+    $this->entityManager->flush();
+
+    $token = $this->tokenManager->create($data);
+
+    return new JsonResponse(['token' => $token]);
   }
 }
---
+++ src/Security/TokenAuthenticator.php
@@ -0,0 +1,35 @@
+<?php
+
+
+namespace App\Security;
+
+
+use Lexik\Bundle\JWTAuthenticationBundle\Exception\ExpiredTokenException;
+use Lexik\Bundle\JWTAuthenticationBundle\Security\Authentication\Token\PreAuthenticationJWTUserToken;
+use Lexik\Bundle\JWTAuthenticationBundle\Security\Guard\JWTTokenAuthenticator;
+use Symfony\Component\Security\Core\User\UserProviderInterface;
+
+class TokenAuthenticator extends JWTTokenAuthenticator
+{
+  /**
+   * @param PreAuthenticationJWTUserToken $preAuthToken
+   * @param UserProviderInterface $userProvider
+   * @return \Symfony\Component\Security\Core\User\UserInterface|void
+   */
+  public function getUser($preAuthToken, UserProviderInterface $userProvider)
+  {
+    /**
+     * @vra User $user
+     */
+    $user = parent::getUser($preAuthToken, $userProvider);
+
+    if($user->getPasswordChangeDate() &&
+      $preAuthToken->getPayload()['iat'] < $user->getPasswordChangeDate()
+    ) {
+      throw new ExpiredTokenException();
+    }
+
+    return $user;
+  }
+
+}

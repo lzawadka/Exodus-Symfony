d3428d6d45c6c44e4d1eb6946465ac7f48817d0f
zawadka <louis.78100@hotmail.fr>
2020-06-30 10:06:23 +0200

    Update UploadFile

--- public/index.php
+++ public/index.php
@@ -15,6 +15,25 @@ if ($_SERVER['APP_DEBUG']) {
     Debug::enable();
 }
 
+$requestMethod = $_SERVER['REQUEST_METHOD'];
+
+// Is this a pre-flight request (the request method is OPTIONS)? Then start output buffering.
+if ($requestMethod === 'OPTIONS') {
+  ob_start();
+}
+
+// Allow for all origins and credentials. Also allow GET, POST, PATCH, and OPTIONS request verbs
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Credentials: true');
+header('Access-Control-Allow-Headers: Access-Control-Allow-Headers, Authorization, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
+header('Access-Control-Allow-Methods: GET, POST, PATCH, OPTIONS, DELETE');
+
+// If this is a pre-flight request (the request method is OPTIONS)? Then flush the output buffer and exit.
+if ($requestMethod === 'OPTIONS') {
+  ob_end_flush();
+  exit();
+}
+
 if ($trustedProxies = $_SERVER['TRUSTED_PROXIES'] ?? false) {
     Request::setTrustedProxies(explode(',', $trustedProxies), Request::HEADER_X_FORWARDED_ALL ^ Request::HEADER_X_FORWARDED_HOST);
 }
